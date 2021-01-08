a304d334bfd58114f610f753f0f72b5c8742c091
Domingos Lemos <dalemos1@gmail.com>
2021-01-08 16:22:45 +0000

    c

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ configure_uploads(app, photos)
 patch_request_class(app)  # set maximum file size, default is 16MB
 
 # Load the model:
-cnn_model = load_model('food_baseline_model.h5')
+cnn_model = load_model('best_model_3class_final.h5')
 CLASS_INDICES = ['apple_pie', 'baby_back_ribs', 'baklava', 'beef_carpaccio', 'beef_tartare', 'beet_salad', 'beignets', 'bibimbap',
        'bread_pudding', 'breakfast_burrito', 'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake',
        'ceviche', 'cheese_plate', 'cheesecake', 'chicken_curry', 'chicken_quesadilla', 'chicken_wings', 'chocolate_cake',
